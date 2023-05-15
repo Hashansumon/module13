5b183cc741e5c49cdb40b3c9c46faf1b7676802c
HM Sumon77 <hmfussumon7707@gmail.com>
2023-05-15 22:52:36 +0600

    assignment

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Contact Page</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css">
+</head>
+<body class="px-5 pt-3 mb-3">
+    <h1>This is contact page</h1>
+    <label for="">Name</label>
+    <input type="text" name="name" class="form-control" placeholder="your name">
+    <label for="">Email</label>
+    <input type="text" name="email" class="form-control mb-3" placeholder="your email">
+    <button class="btn btn-primary mb-3 py-3">Click</button>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
+
